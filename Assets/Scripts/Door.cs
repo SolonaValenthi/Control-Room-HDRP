bc2ce4fe84c036e9dfc22bd08322d9f0aca75e7e
Nikolai Nelson <nikolainelson@comcast.net>
2023-12-14 03:05:19 -0800

    prepping for functional door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
+}
