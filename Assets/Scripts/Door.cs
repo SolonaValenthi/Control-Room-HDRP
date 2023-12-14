036fc9c3d6bf47a8105ef611256a8aa814b35396
Nikolai Nelson <nikolainelson@comcast.net>
2023-12-14 03:18:55 -0800

    main doorway now opens when the player approaches it

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     Animator _animator;
+    BoxCollider[] _physicsCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _physicsCollider = GetComponents<BoxCollider>();
+
+        if (_animator == null)
+        {
+            Debug.LogError("Failed to cache door animator on " + this.gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +29,17 @@ public class Door : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            _animator.SetBool("Open", true);
+            _physicsCollider[0].enabled = false;
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("Open", false);
+            _physicsCollider[0].enabled = true;
         }
     }
 }
